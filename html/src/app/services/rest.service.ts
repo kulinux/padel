ad658e5b972b14f6a67432e91b6dd7e0c7a36843
Francisco Jose Benitez Lazaro <paco.benitez.lazaro@gmail.com>
2018-04-14 00:00:25 +0200

    http

--- html/src/app/services/rest.service.ts
+++ html/src/app/services/rest.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
+import { isDevMode } from '@angular/core';
+import { Subject } from "rxjs/Subject";
+
 
 import { MatchResult, Tournament } from '../model/model';
 
@@ -13,15 +16,23 @@ export class RestService {
 
   private getTournamentUrl: string = "/padel/tournaments/666";
   private winMatchUrl: string = "/padel/tournaments/666/winMatch";
+  private mockMatchUrl = new Subject<string>();
+  private mockTournament = new Subject<Tournament>();
 
   constructor(private http: HttpClient) { }
 
   getTournament(): Observable<Tournament> {
+    if(isDevMode()) {
+      return this.mockTournament.asObservable();
+    }
     return this.http.get<Tournament>(this.getTournamentUrl)
   }
 
-  win(match: MatchResult) {
-    return this.http.post<MatchResult>(this.winMatchUrl, match, httpOptions)
+  win(match: MatchResult) : Observable<string> {
+    if(isDevMode()) {
+      return this.mockMatchUrl.asObservable();
+    }
+    return this.http.post<string>(this.winMatchUrl, match, httpOptions)
   }
 
 }
