8e824536eeea4bb75b0f2a35eeddeb0df776037a
Francisco Jose Benitez Lazaro <paco.benitez.lazaro@gmail.com>
2018-05-31 21:56:27 +0200

    redux

--- html/src/index.js
+++ html/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import store from './store';
+import routes from './routes';
+
+import './styles/main.css';
+
+ReactDOM.render(
+  <Provider store={store}>
+    {routes}
+  </Provider>,
+  document.getElementById('root'),
+);
